0ccd10fc3248d73ed95fcbe477ae98424d9f2565
Liviu Manea <mlmarius@yahoo.com>
2018-09-14 14:03:00 +0300

    fixing services endpoint showing same json schema for all endpoints

--- src/eposfederator/webapi/handlers/services.py
+++ src/eposfederator/webapi/handlers/services.py
@@ -1,6 +1,7 @@
 from eposfederator.libs.base.requesthandler import RequestHandler
 from eposfederator.webapi.libs import config
 from marshmallow_jsonschema import JSONSchema
+import logging
 
 
 class MapHandler(RequestHandler):
@@ -12,13 +13,14 @@ class MapHandler(RequestHandler):
 
     def get(self):
         output = {}
-        json_schema = JSONSchema()
         for plugin_id, plugin in config.APPCONFIG['plugins'].items():
             output[plugin_id] = {}
             for handler in plugin.HANDLERS:
                 schema = handler.REQUEST_SCHEMA()
+                logging.info(schema)
+                logging.info(id(schema))
                 output[plugin_id][handler.ID] = {
                     "route": (plugin.BASE_ROUTE + '/' + handler.ROUTE).rstrip('/'),
-                    "request_schema": json_schema.dump(schema)
+                    "request_schema": JSONSchema().dump(schema)
                 }
         self.write(output)
