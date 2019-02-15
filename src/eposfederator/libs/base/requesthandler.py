20318d8515f6990fc79e0734fbaeaa21509c5dfb
Liviu Manea <mlmarius@yahoo.com>
2019-02-15 12:13:18 +0100

    adding request validation via request headers or oauth token(incomplete)

--- src/eposfederator/libs/base/requesthandler.py
+++ src/eposfederator/libs/base/requesthandler.py
@@ -1,10 +1,27 @@
 import tornado
 import logging
+import tornado.web
 
 
 class RequestHandler(tornado.web.RequestHandler):
 
     def prepare(self):
+
+        # try to get the OAUTH token
+        token = self.get_argument("token", None)
+        # TODO: validate token with regular expression! What does the token look like ?!?
+        if token is not None:
+            # now validate token
+            token_valid = True
+
+            if token_valid is False:
+                raise tornado.web.HTTPError(404)
+
+        else:
+            fridge_authorization = self.request.headers.get('Fridge-Authorize', None)
+            if fridge_authorization != "FDerkaeriASFLK":
+                raise tornado.web.HTTPError(404)
+
         logging.info("Response type should be {}".format(self.RESPONSE_TYPE))
         self.set_header('Content-Type', self.RESPONSE_TYPE)
 
