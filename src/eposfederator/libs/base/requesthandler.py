c60077f762616fa02d962f4d1807afab60ed5230
Ivano Carluccio <ivano.carluccio@ingv.it>
2019-04-29 17:38:42 +0200

    AAI token validation

--- src/eposfederator/libs/base/requesthandler.py
+++ src/eposfederator/libs/base/requesthandler.py
@@ -1,26 +1,49 @@
 import tornado
 import logging
 import tornado.web
+from urllib.request import urlopen
+from urllib.request import Request
+import re
+
+
+class TokenValidation():
+    # token validation: should match "Bearer <token>"
+    def token_validation(token):
+        resp = None
+        # fixed length token (43 chars)
+        if token is not None and re.match("Bearer [a-zA-Z0-9_-]{43}$", token):
+            resp = token
+        return resp
 
 
 class RequestHandler(tornado.web.RequestHandler):
-
     def prepare(self):
-
+        # auth-service (or whatever): google, facebook, edugain, ...
+        # auth_service = self.request.headers.get('auth-service', None)
         # try to get the OAUTH token
-        token = self.get_argument("token", None)
-        # TODO: validate token with regular expression! What does the token look like ?!?
-        if token is not None:
+        btoken = self.request.headers.get('Authorization', None)  # "Bearer <token>"
+        if TokenValidation.token_validation(btoken) is not None:
             # now validate token
-            token_valid = True
+            auth_url = "https://aaai.epos-eu.org/oauth2/userinfo"
+            # set header : "Authorization: Bearer <token>"
+            authreq = Request(auth_url, headers={"Authorization": btoken})
+            authresp = urlopen(authreq)
+            
+            token_valid = False
+            if authresp.getcode() == 200:
+                token_valid = True
+            
+            # read userinfo for accounting
+            # {sub, lastname, firstname, eduPersonUniqueId, email}
+            # user_obj = authresp.read()
 
             if token_valid is False:
-                raise tornado.web.HTTPError(404)
+                raise tornado.web.HTTPError(401)
 
         else:
             fridge_authorization = self.request.headers.get('Fridge-Authorize', None)
             if fridge_authorization != "FDerkaeriASFLK":
-                raise tornado.web.HTTPError(404)
+                raise tornado.web.HTTPError(403)
 
         logging.info("Response type should be {}".format(self.RESPONSE_TYPE))
         self.set_header('Content-Type', self.RESPONSE_TYPE)
